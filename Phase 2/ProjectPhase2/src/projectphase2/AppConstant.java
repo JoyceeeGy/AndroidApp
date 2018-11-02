6542443a3aafb3c01b600f0844883f61de704dd7
Yue Gong <joyce.yue@hotmail.com>
2018-11-01 21:15:13 -0400

    add who project phase 2

---
+++ Phase 2/ProjectPhase2/src/projectphase2/AppConstant.java
@@ -0,0 +1,9 @@
+package projectphase2;
+
+public class AppConstant {
+	
+	// File path that stores Patients' records
+	public static final String P_RECORDS = "src/projectphase2/patient_records.txt";
+	// File path that stores Patients' visit records
+	public static final String V_RECORDS = "src/projectphase2/visit_records.txt";
+}
